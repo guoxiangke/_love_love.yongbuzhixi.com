f8b9eec265006ef75df4e50fe2341b4c49e2c91f
xiangkeguo@gmail.com <xiangkeguo@gmail.com>
2013-12-19 21:20:34 +0800

    debug3

--- sites/all/modules/mp_service/wechat.class.php
+++ sites/all/modules/mp_service/wechat.class.php
@@ -166,7 +166,7 @@ class Wechat
 	public function getRev()
 	{
 		$postStr = file_get_contents("php://input");
-		$this->log($postStr);watchdog('getRev', '<pre>:'.print_r($postStr,TRUE), array(), WATCHDOG_NOTICE, 'link');
+		$this->log($postStr);
 		if (!empty($postStr)) {
 			$this->_receive = (array)simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
 		}
